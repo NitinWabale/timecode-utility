6bded7fd87a20d078d44267d9d7a3e5a31f55b7c
Nitin <nwabale@alegion.com>
2021-03-02 14:24:25 +0800

    GREEN: Added implementation for conversion

--- src/timecode-utility.ts
+++ src/timecode-utility.ts
@@ -1,6 +1,11 @@
 // convert timecode to frames
 export const convertTimeCodeToFrames = (timecode: string, frameRate: number) => {
-   return 0;
+    const timecodeArray = timecode.split(':');
+    const hours = parseInt(timecodeArray[0], 10);
+    const minutes = parseInt(timecodeArray[1], 10);
+    const seconds = parseInt(timecodeArray[2], 10);
+    const frames = parseInt(timecodeArray[3], 10);
+    return (hours * 3600 + minutes * 60 + Math.trunc(seconds)) * Math.trunc(frameRate) + Math.trunc(frames);
   };
   
   // pad the 0 if number less than 10
@@ -8,5 +13,12 @@ export const convertTimeCodeToFrames = (timecode: string, frameRate: number) => 
   
   // convert frames to time code
   export const convertFramesToTimeCode = (frames: number, fps: number) => {
-    return ""
+    const seconds = Math.trunc(frames / Math.trunc(fps));
+  
+    return [
+      pad(Math.trunc(seconds / 3600)),
+      pad(Math.trunc((seconds % 3600) / 60)),
+      pad(Math.trunc(seconds % 60)),
+      pad(Math.trunc(frames % Math.trunc(fps))),
+    ].join(':');
   };
